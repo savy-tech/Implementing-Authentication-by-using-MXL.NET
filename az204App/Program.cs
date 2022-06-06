d62dca97f6b4bc67a636276518ece8985dbea680
savy-tech <50862935+savy-tech@users.noreply.github.com>
2022-06-06 10:09:18 +0100

    Add files via upload

---
+++ az204App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks; 
+using Microsoft.Identity.Client;
+
+namespace az204App
+{
+    class Program
+    {
+        private const string _clientId = "08c1cbf5-4755-43cf-bdf1-61dfc2d2c901";
+       
+        private const string _tenantId = "97e84c6d-d397-437d-91f5-240646017107";
+        
+        public static async Task Main(string[] args)
+        
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId) 
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost") 
+                .Build();
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+            
+            Console.WriteLine("I am in love with A.Z204 !");
+        }
+    }
+}
